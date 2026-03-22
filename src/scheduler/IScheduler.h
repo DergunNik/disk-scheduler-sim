47ca602dcc3e35f78307b929309703fa943cf639
Dziarhun Mikalai <dergunnik@gmail.com>
2026-03-22 17:14:38 +0300

    feat: add scheduler interface

---
+++ src/scheduler/IScheduler.h
@@ -0,0 +1,26 @@
+﻿#ifndef DISK_SCHEDULER_SIM_ISCHEDULER_H
+#define DISK_SCHEDULER_SIM_ISCHEDULER_H
+
+#include <cstddef>
+#include <optional>
+#include <vector>
+
+#include "../request/Request.h"
+#include "../disk/DiskState.h"
+
+namespace disksim::scheduling {
+
+    class IScheduler {
+    public:
+        virtual ~IScheduler() = default;
+
+        virtual std::optional<std::size_t> pickNext(
+            const std::vector<Request>& requests,
+            const DiskState& state,
+            int now
+        ) const = 0;
+    };
+
+}
+
+#endif // DISK_SCHEDULER_SIM_ISCHEDULER_H
